ce5448e28363d656445adee95eba0e697fc51c01
ZAF07 <zaffere07@gmail.com>
2021-06-08 12:22:20 +0800

    removed profile and reset all env vars for development

--- model/db.js
+++ model/db.js
@@ -1,26 +1,13 @@
 import pg from 'pg';
  const { Pool } = pg;
-let pgConnectionConfig;
 
 // DB Configuration
-
-
-if (process.env.DATABASE_URL) {
-  pgConnectionConfig = {
-    connectionString: process.env.DATABASE_URL,
-    ssl: {
-      rejectUnauthorized: false,
-    },
-  };
-} else {
-   
-     pgConnectionConfig = {
+const pgConnectionConfig = {
       user: process.env.DB_USER,
       host: process.env.DB_HOST,
       database: process.env.DB_NAME,
       port: process.env.DB_PORT,
     };
-}
 
 
 const pool = new Pool(pgConnectionConfig);
