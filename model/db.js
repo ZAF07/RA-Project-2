6de0b7cf7b0432e4da8395810f64f62ab64f146d
ZAF07 <zaffere07@gmail.com>
2021-06-09 11:52:24 +0800

    edit indentation

--- model/db.js
+++ model/db.js
@@ -1,14 +1,14 @@
 import pg from 'pg';
- const { Pool } = pg;
+
+const { Pool } = pg;
 
 // DB Configuration
 const pgConnectionConfig = {
-      user: process.env.DB_USER,
-      host: process.env.DB_HOST,
-      database: process.env.DB_NAME,
-      port: process.env.DB_PORT,
-    };
-
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
+};
 
 const pool = new Pool(pgConnectionConfig);
 
